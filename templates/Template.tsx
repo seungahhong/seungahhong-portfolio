3c498fa082616854f7002ea4cca9ae365af9d987
hongseungah <gmm117@naver.com>
2022-11-26 18:13:10 +0900

    main feat: 포트폴리오 기능추가
     - 포트폴리오 meta og,twitter image 추가

--- templates/Template.tsx
+++ templates/Template.tsx
@@ -32,12 +32,20 @@ const Template: FunctionComponent<TemplateProps> = ({
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:url" content={url} />
+        <meta
+          property="og:image"
+          content={`${url}/_next/image?url=%2Fworks_portfolio_1_logo.png&w=3840&q=75`}
+        />
         <meta property="og:site_name" content={author} />
 
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:site" content={author} />
+        <meta
+          property="twitter:image"
+          content={`${url}/_next/image?url=%2Fworks_portfolio_1_logo.png&w=3840&q=75`}
+        />
         <meta name="twitter:creator" content={author} />
 
         <meta
