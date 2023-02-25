5f2d77bcea2e31b58cdb592fcdef3658cce12e43
hongseungah <gmm117@naver.com>
2023-02-25 19:29:19 +0900

    feature/playwright feat: e2e test 추가
     - e2e test tool playwright 환경설정 수정
     - e2e mobile/desktop일 경우에 대한 테스트 코드 수정

--- e2e/playwright/00-main-home.test.ts
+++ e2e/playwright/00-main-home.test.ts
@@ -1,31 +1,20 @@
 import { expect, Page, test, BrowserContext } from '@playwright/test';
 
 test.describe('포트폴리오 Main 홈', () => {
-  let browserContext: BrowserContext;
-  let page: Page;
-
-  // beforeAll hook은 최초 딱 한번 실행. initialize 작업 등을 수행
-  test.beforeAll(async ({ browser, contextOptions }) => {
-    browserContext = await browser.newContext(contextOptions);
-  });
-
-  test.beforeEach(async () => {
-    // 페이지 생성
-    page = await browserContext.newPage();
-
+  test.beforeEach(async ({ page, context, baseURL }) => {
     await page.goto('/');
   });
 
-  test('00-1. document title이 올바르다', async () => {
+  test('00-1. document title이 올바르다', async ({ page }) => {
     // document.title이 올바른지 확인
     await expect(page).toHaveTitle('홍승아 포트폴리오');
   });
 
-  test('00-2. Navigation 존재하는지를 확인한다.', async () => {
+  test('00-2. Navigation 존재하는지를 확인한다.', async ({ page }) => {
     await expect(page.locator('header ul > li')).toHaveCount(4);
   });
 
-  test('00-3. 소셜 버튼이 존재하는지를 확인한다.', async () => {
+  test('00-3. 소셜 버튼이 존재하는지를 확인한다.', async ({ page }) => {
     const element = page
       .locator('div > a[title="핸드폰번호"]')
       .first()
@@ -35,7 +24,7 @@ test.describe('포트폴리오 Main 홈', () => {
     await expect(element).toHaveCount(4);
   });
 
-  test('00-4. footer가 존재하는지를 확인한다.', async () => {
+  test('00-4. footer가 존재하는지를 확인한다.', async ({ page }) => {
     await expect(page.locator('footer')).toBeVisible();
   });
 });
