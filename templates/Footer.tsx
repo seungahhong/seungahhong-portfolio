2c98d428a0b550081940adb21b7549472c5ea645
hongseungah <gmm117@naver.com>
2023-10-09 23:20:47 +0900

    develop refactor: 포트폴리오 리팩토링
      - footer 컴포넌트 tailwind css 스타일 적용

--- templates/Footer.tsx
+++ templates/Footer.tsx
@@ -1,24 +1,12 @@
 import { FunctionComponent } from 'react';
-import styled from '@emotion/styled';
-
-const Container = styled.footer`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 13px;
-  color: #abb7b7;
-  line-height: 20px;
-  padding: ${(props) => props.theme.spacing['spacing-5']};
-  border-top: 1px solid #dee2e6;
-`;
 
 const Footer: FunctionComponent = () => {
   return (
-    <Container>
+    <footer className="flex justify-center items-center text-xs leading-5 text-[#abb7b7] border-t-0 border-solid p-4">
       Thank You for Visiting HongSeungAh Portfolio,
       <br />
       Powered By NextJs.
-    </Container>
+    </footer>
   );
 };
 
