6705360e5d95e05957ecb763c72323352c03bb03
hongseungah <gmm117@naver.com>
2023-11-12 00:14:50 +0900

    develop refactor: 포트폴리오 리팩토링
     - carrer, work 컴포넌트에 tailwind css로 변경
     - 전역변수, custom css 추가적으로 선언

--- pages/career/index.tsx
+++ pages/career/index.tsx
@@ -1,39 +1,10 @@
-import styled from '@emotion/styled';
-import facepaint from 'facepaint';
-
 import { careerProjectValues } from '../../helpers/datas/career';
-import { breakpoints } from '../../helpers/styles/mediaQuery';
 import PageHeader from '../../templates/components/PageHeader';
 import CardA from '../../components/Cards/CardA';
 import useInfinityScroll from '../../helpers/hooks/useInfinityScroll';
 import useMediaQuery from '../../helpers/hooks/useMediaQuery';
 import { CardsType } from '../../components/type';
 import { NextPageWithLayout } from '../_app';
-
-const mq = facepaint(breakpoints.map((bp) => `@media (max-width: ${bp}px)`));
-
-const Container = styled.section`
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  max-width: 1024px;
-  padding: 96px 0 60px;
-  margin: 0 auto;
-`;
-
-const List = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  flex: 0 1 auto;
-  box-sizing: border-box;
-  margin-top: ${(props) => props.theme.spacing['spacing-6']};
-
-  & > li {
-    ${mq({
-      width: ['calc(4 / 12 * 100%)', '100%'],
-    })}
-  }
-`;
 
 const Career: NextPageWithLayout = () => {
   const isMobile = useMediaQuery();
@@ -47,13 +18,17 @@ const Career: NextPageWithLayout = () => {
       <PageHeader title="경력" />
       {cards.length > 0 && (
         <>
-          <List>
+          <ul className="flex flex-wrap flex-initial mt-[var(--spacing-6)]">
             {cards.map((card, index) => (
-              <li key={`CareerCard_${index}`} role="none">
+              <li
+                key={`CareerCard_${index}`}
+                role="none"
+                className="w-[100%] lg:w-[calc(4_/_12_*_100%)]"
+              >
                 <CardA {...card} href={`/career/${card.href}`} />
               </li>
             ))}
-          </List>
+          </ul>
           <div ref={currentRef} />
         </>
       )}
@@ -62,7 +37,11 @@ const Career: NextPageWithLayout = () => {
 };
 
 Career.getLayout = function getLayout(page: React.ReactElement) {
-  return <Container>{page}</Container>;
+  return (
+    <section className="flex flex-col flex-1 max-w-[1024px] pt-[96px] pb-[60px] my-[auto]">
+      {page}
+    </section>
+  );
 };
 
 export default Career;
