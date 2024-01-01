3e11422061fb69aca647bf5c2473230e0f8b2d43
hongseungah <gmm117@naver.com>
2024-01-01 15:00:26 +0900

    develop refactor: 포트폴리오 리팩토링
     - GA4 pageview 연동, 타입 에러 수정

--- app/templates/GoogleAnalytics.tsx
+++ app/templates/GoogleAnalytics.tsx
@@ -1,24 +1,43 @@
-import Script from 'next/script';
+'use client';
 
-const GoogleAnalytics = ({ gaID }: { gaID: string }) => (
-  <>
-    <Script
-      async
-      src={`https://www.googletagmanager.com/gtag/js? 
-      id=${gaID}`}
-     />
-    <Script
-      id="google-analytics"
-      dangerouslySetInnerHTML={{
-        __html: `
+import { useEffect } from 'react';
+import { usePathname } from 'next/navigation';
+import Script from 'next/script';
+import { pageview } from '../../lib/gtags';
+
+const GoogleAnalytics = ({ gaID }: { gaID: string }) => {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (pathname && process.env.NODE_ENV !== 'development') {
+      pageview(pathname);
+    }
+  }, [pathname]);
+
+  if (process.env.NODE_ENV === 'development') {
+    return null;
+  }
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gaID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
           gtag('config', '${gaID}');
         `,
-      }}
-     />
-  </>
-);
+        }}
+      />
+    </>
+  );
+};
 export default GoogleAnalytics;
--- lib/gtags.ts
+++ lib/gtags.ts
@@ -1,15 +1,21 @@
-export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID as string;
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-export const pageview = (url: string) => {
-  window.gtag('config', GA_TRACKING_ID, {
+export const pageview = (url: URL | string) => {
+  window.gtag('config', GA_TRACKING_ID as string, {
     page_path: url,
   });
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const event = ({ action, category, label, value }: any) => {
+type GTagEvent = {
+  action: string;
+  category: string;
+  label: string;
+  value: number;
+};
+
+export const event = ({ action, category, label, value }: GTagEvent) => {
   window.gtag('event', action, {
     event_category: category,
     event_label: label,
