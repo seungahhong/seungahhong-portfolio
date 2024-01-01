8481a2999cd8184d1be6d17b54d9f02ea1d6a4b5
hongseungah <gmm117@naver.com>
2024-01-01 15:42:48 +0900

    develop refactor: 포트폴리오 리팩토링
     - eslint 제외 파일 추가

--- types/carousel.ts
+++ types/carousel.ts
@@ -1,4 +1,5 @@
 /** 방향타입: 왼쪽(LEFT), 오른쪽(RIGHT), 위쪽(TOP), 아래쪽(BOTTOM) */
+// eslint-disable-next-line import/prefer-default-export
 export const DIRECTION_TYPE = {
   LEFT: 'LEFT',
   RIGHT: 'RIGHT',
