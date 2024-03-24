5ba6365d6d35993175eeaad43bcb9558c8021a95
hongseungah <gmm117@naver.com>
2024-03-24 20:24:34 +0900

    develop feat: 포트폴리오 리팩토링
     - 회사 카테고리 SSG 도입

--- app/career/[slug]/page.tsx
+++ app/career/[slug]/page.tsx
@@ -1,12 +1,11 @@
-'use client';
-
 import { careerProjectDetailType } from '../../../helpers/datas/career';
-import { IProjectItem } from '../../../types';
-import useInfinityScroll from '../../../helpers/hooks/useInfinityScroll';
-import useMediaQuery from '../../../helpers/hooks/useMediaQuery';
 import Header from '../../components/Header';
 
 import Project from '../../components/Project';
+
+export async function generateStaticParams() {
+  return Object.keys(careerProjectDetailType).map((key) => [{ slug: key }]);
+}
 
 const CareerItem = ({
   params,
@@ -16,29 +15,20 @@ const CareerItem = ({
 }) => {
   const { header, items } = careerProjectDetailType[params.slug];
 
-  const isMobile = useMediaQuery();
-  const [currentRef, datas] = useInfinityScroll<IProjectItem>(
-    items,
-    isMobile ? 1 : 2,
-  );
   return (
     <div className="flex flex-col w-[100%] bg-[#f5f5f5] text-base py-[96px]">
       <Header title={header} />
       {items.length > 0 && (
-        <>
-          <section className="width-[100%] p-4 lg:p-12">
-            {datas.map((item, index) => (
-              <Project
-                key={`CareerItem_${index}`}
-                item={item}
-                style={{ marginTop: index > 0 ? '36px' : 0 }}
-              />
-            ))}
-          </section>
-          <div ref={currentRef} />
-        </>
+        <section className="width-[100%] p-4 lg:p-12">
+          {items.map((item, index) => (
+            <Project
+              key={`CareerItem_${index}`}
+              item={item}
+              style={{ marginTop: index > 0 ? '36px' : 0 }}
+            />
+          ))}
+        </section>
       )}
-      <div ref={currentRef} />
     </div>
   );
 };
--- app/components/Carousel/Carousel.tsx
+++ app/components/Carousel/Carousel.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 /* eslint-disable no-nested-ternary, no-unsafe-optional-chaining */
 import {
   FunctionComponent,
